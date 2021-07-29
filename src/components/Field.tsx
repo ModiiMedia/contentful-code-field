5f7777044ae4b39f1556533b0fdd5353d10225c7
Josh Michaels <31548851+joshmossas@users.noreply.github.com>
2021-07-29 14:52:13 -0500

    create monaco editor field

--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
-import { Paragraph } from '@contentful/forma-36-react-components';
 import { FieldExtensionSDK } from '@contentful/app-sdk';
+import Editor from '@monaco-editor/react';
 
 interface FieldProps {
   sdk: FieldExtensionSDK;
@@ -10,7 +9,9 @@ const Field = (props: FieldProps) => {
   // If you only want to extend Contentful's default editing experience
   // reuse Contentful's editor components
   // -> https://www.contentful.com/developers/docs/extensibility/field-editors/
-  return <Paragraph>Hello Entry Field Component</Paragraph>;
+  return <Editor height="84vh" defaultLanguage="html" value={props.sdk.field.getValue()} onChange={(val) => {
+    props.sdk.field.setValue(val);
+  }}/>;
 };
 
 export default Field;
